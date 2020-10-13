5cf1ffc1ad5ebaffef67eb505dd2d408bf9c7121
Gustavo Lahr <gustavo.lahr@usp.br>
2020-10-13 10:45:47 -0300

    added action server without feedback for cartesian pose control

--- iiwa_tool/iiwa_tool_examples/scripts/joint_pos_ctrl_test_action.py
+++ iiwa_tool/iiwa_tool_examples/scripts/joint_pos_ctrl_test_action.py
@@ -4,7 +4,8 @@ import time
 from math import pi
 import rospy
 
-from iiwa_msgs.msg import CartesianPose, MoveToCartesianPoseGoal, MoveToCartesianPoseAction
+from iiwa_msgs.msg import CartesianPose, MoveToJointPositionGoal, MoveToJointPositionAction
+# from iiwa_msgs.action import MoveToJointPosition
 from geometry_msgs.msg import PoseStamped
 import actionlib
 
@@ -64,46 +65,34 @@ def pose_publisher():
         # print("\n\n\n\n x_1: \n", x_1)
         print("\n\n\n\n curr_pos: \n", states.current_pos)
 
-def cart_action():
-    robot = actionlib.SimpleActionClient('/iiwa/action/move_to_cartesian_pose', MoveToCartesianPoseAction)
+def joint_action():
+    robot = actionlib.SimpleActionClient('/iiwa/action/move_to_joint_position', MoveToJointPositionAction)
 
     print("waiting for server")
     robot.wait_for_server()
 
     try:
-        cart_pos_goal_0 = MoveToCartesianPoseGoal()
-        cart_pos_goal_1 = MoveToCartesianPoseGoal()
-        cart_pos_goal_2 = MoveToCartesianPoseGoal()
-
-        cart_pos_goal_0.cartesian_pose.poseStamped.pose.position.z = 1.3
-        cart_pos_goal_0.cartesian_pose.poseStamped.pose.orientation.w = 1
-
-        cart_pos_goal_1.cartesian_pose.poseStamped.pose.position.x = 0.2
-        cart_pos_goal_1.cartesian_pose.poseStamped.pose.position.z = 1.0
-        cart_pos_goal_1.cartesian_pose.poseStamped.pose.orientation.w = 1
-
-        cart_pos_goal_2.cartesian_pose.poseStamped.pose.position.x = -0.2
-        cart_pos_goal_2.cartesian_pose.poseStamped.pose.position.z = 1.0
-        cart_pos_goal_2.cartesian_pose.poseStamped.pose.orientation.w = 1
-
         while not rospy.is_shutdown():
 
-            print("sending goal 0")
-            robot.send_goal(cart_pos_goal_0)
+            joint_pos_goal = MoveToJointPositionGoal()
+
+            print("sending goal")
+            robot.send_goal(joint_pos_goal)
+
+            robot.wait_for_result()
+            # rate = rospy.Rate(.25) # 0.25 Hz => 4 s
+            print("Goal achieved")
+
+            joint_pos_goal.joint_position.position.a1 = 30*pi/180
+            joint_pos_goal.joint_position.position.a4 = 60*pi/180
+
+            # q_1.position.a1 = 30*pi/180
+            # q_1.position.a4 = 60*pi/180
+            print("sending goal")
+            robot.send_goal(joint_pos_goal)
 
             robot.wait_for_result()
             print("Goal achieved")
-
-            print("sending goal 1")
-            robot.send_goal(cart_pos_goal_1)
-            robot.wait_for_result()
-            print("Goal achieved")
-
-            print("sending goal 2")
-            robot.send_goal(cart_pos_goal_2)
-            robot.wait_for_result()
-            print("Goal achieved")
-
             rate = rospy.Rate(1) # 1 Hz => 1 s
     except KeyboardInterrupt:
         robot.cancel_goal()
@@ -114,12 +103,12 @@ def cart_action():
 if __name__ == "__main__":
 
     # pose_pub = rospy.Publisher('/iiwa/command/CartesianPose', PoseStamped, queue_size = 10)
-    rospy.init_node('cart_pos_ctrl_client', anonymous=True)
+    rospy.init_node('joint_pos_ctrl_client', anonymous=True)
 
     # pos_init =
 
     try:
         #pose_publisher()
-        cart_action()
+        joint_action()
     except KeyboardInterrupt:
         print("Stopping controller...")
